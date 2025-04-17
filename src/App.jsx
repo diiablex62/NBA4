2f025aa4615e8d9c102ca69bcac759ef3cb89b05
Aleexj <jalias59@outlook.fr>
2025-04-17 19:34:43 +0200

    optimisation app et route

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,14 @@
 import { useContext } from "react";
-import { Routes, Route, useLocation, useNavigate } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 import styles from "./App.module.scss";
 import Header from "./components/Header/Header";
 import Loading from "./components/Loading/Loading";
-import Team from "./pages/Teams/Team";
-import Users from "./pages/Users/Users";
-import Favorites from "./pages/Favorites/Favorites";
-import Login from "./pages/Login";
+import AppRoutes from "./routes"; 
 import { AppContext } from "./context/AppContextInstance";
 
 function App() {
-  const location = useLocation(); 
-  const navigate = useNavigate(); 
+  const location = useLocation();
+  const navigate = useNavigate();
   const {
     teams,
     isLoading,
@@ -47,49 +44,20 @@ function App() {
       {isLoading ? (
         <Loading />
       ) : (
-        <Routes>
-          <Route
-            path='/'
-            element={
-              <Team
-                person={{ name: "Alex", age: 31 }}
-                teams={teams}
-                licensed={licensed}
-                login={login}
-                view={view}
-                filter={filter}
-                handleTogglePages={handleTogglePages}
-                toggleLiked={toggleLiked}
-              />
-            }
-          />
-          <Route path='/login' element={<Login />} />
-          <Route
-            path='/users'
-            element={
-              <Users
-                showUsers={handleTogglePages}
-                handleClick={handleClick}
-                handleInputUser={handleInputUser}
-                user={user}
-                allUsers={allUsers}
-                handleClickDelete={handleClickDelete}
-                licensed={licensed}
-              />
-            }
-          />
-          <Route
-            path='/favorites'
-            element={
-              <Favorites
-                showUsers={handleTogglePages}
-                teams={teams}
-                toggleLiked={toggleLiked}
-                licensed={licensed}
-              />
-            }
-          />
-        </Routes>
+        <AppRoutes
+          teams={teams}
+          licensed={licensed}
+          login={login}
+          view={view}
+          filter={filter}
+          handleTogglePages={handleTogglePages}
+          toggleLiked={toggleLiked}
+          handleClick={handleClick}
+          handleInputUser={handleInputUser}
+          user={user}
+          allUsers={allUsers}
+          handleClickDelete={handleClickDelete}
+        />
       )}
     </div>
   );
--- src/routes.jsx
+++ src/routes.jsx
@@ -1,14 +1,67 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import React from "react";
+import { Routes, Route } from "react-router-dom";
 import Team from "./pages/Teams/Team";
-import Login from "./pages/Login"; 
+import Users from "./pages/Users/Users";
+import Favorites from "./pages/Favorites/Favorites";
+import Login from "./pages/Login";
 
-export default function AppRoutes() {
+export default function AppRoutes({
+  teams,
+  licensed,
+  login,
+  view,
+  filter,
+  handleTogglePages,
+  toggleLiked,
+  handleClick,
+  handleInputUser,
+  user,
+  allUsers,
+  handleClickDelete,
+}) {
   return (
-    <Router>
-      <Routes>
-        <Route path='/' element={<Team />} />
-        <Route path='/login' element={<Login />} /> 
-      </Routes>
-    </Router>
+    <Routes>
+      <Route
+        path='/'
+        element={
+          <Team
+            person={{ name: "Alex", age: 31 }}
+            teams={teams}
+            licensed={licensed}
+            login={login}
+            view={view}
+            filter={filter}
+            handleTogglePages={handleTogglePages}
+            toggleLiked={toggleLiked}
+          />
+        }
+      />
+      <Route path='/login' element={<Login />} />
+      <Route
+        path='/users'
+        element={
+          <Users
+            showUsers={handleTogglePages}
+            handleClick={handleClick}
+            handleInputUser={handleInputUser}
+            user={user}
+            allUsers={allUsers}
+            handleClickDelete={handleClickDelete}
+            licensed={licensed}
+          />
+        }
+      />
+      <Route
+        path='/favorites'
+        element={
+          <Favorites
+            showUsers={handleTogglePages}
+            teams={teams}
+            toggleLiked={toggleLiked}
+            licensed={licensed}
+          />
+        }
+      />
+    </Routes>
   );
 }
